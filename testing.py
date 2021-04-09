c0eac1fc85dee22db6d650bcd083c0522c1ac8f7
Mustafa Al-ameen Mofeyisayo <mustafaalameen2017@gmail.com>
2021-04-09 03:38:35 +0100

    returning a dictionary
    This function is used to9 show how a dictionary can be returned from a function.

---
+++ testing.py
@@ -0,0 +1,16 @@
+def build_person(first_name, last_name):
+   person= {"first": first_name,"last": last_name}
+   return person
+build_person("James", "Nice")
+print(build_person("James", "Nice"))
+
+def build_person(first_name, last_name, middle_name=' ', age= None, occupation=' '):
+    person= {"first": first_name,"last": last_name, 'middle': middle_name
+            }
+    if age:
+        person['age']=age
+    if occupation:
+        person['occupation']='Actor'
+    return person
+
+print(build_person("James", "Nice", "Famous", 23, "Actor"))
